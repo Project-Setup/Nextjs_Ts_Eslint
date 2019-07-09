75f6ffa314e8a143f8874dce6d0d357ea88c853d
clfhhc <david.chen@minted.com>
2019-07-09 12:42:59 -0700

    added a simple example

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+};
+
+export default MyApp;
