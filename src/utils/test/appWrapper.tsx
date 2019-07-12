1db252ecaa5100b5fd26a2a9acc9931a7e8901cc
clfhhc <david.chen@minted.com>
2019-07-11 23:15:29 -0700

    changed tests

--- src/utils/test/appWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import App, { Props } from '../../../pages/_app';
-
-const AppWrapper = (props: Props) => mount(<App {...props} />);
-
-export default AppWrapper;
