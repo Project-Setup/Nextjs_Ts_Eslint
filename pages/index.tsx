ebc548543d729b26134622f1a9a10f90cc6d2c5a
clfhhc <david.chen@minted.com>
2019-07-12 01:36:17 -0700

    added example index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { NextPage } from 'next';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
 import Loading from '../src/components/Loading';
 import ManifestHead from '../src/components/Head/ManifestHead';
 import Link from '../src/components/Link';
+import { RootState } from '../src/redux/store';
+import { addNumber, minusNumber } from '../src/redux/actions/actions';
 
-const Page: NextPage = () => (
+const mapStateToProps = ({ count: { count } }: RootState) => ({
+  count,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  addNum: (num: number) => dispatch(addNumber(num)),
+  minusNum: (num: number) => dispatch(minusNumber(num)),
+});
+
+interface Props extends ReturnType<typeof mapStateToProps>, ReturnType<typeof mapDispatchToProps> {}
+
+const Page: NextPage<Props> = ({ count, addNum, minusNum }) => (
   <main>
     <ManifestHead
       title="Nextjs Typescript Eslint"
@@ -15,6 +30,13 @@ const Page: NextPage = () => (
       hrefManifest="/static/manifest/manifest.json"
     />
     <span>This is index</span>
+    <div>{`Number is ${count}`}</div>
+    <button type="button" onClick={() => addNum(2)}>
+      add 2
+    </button>
+    <button type="button" onClick={() => minusNum(3)}>
+      minus 3
+    </button>
     <Loading />
     <Link href="/about" prefetch={false}>
       <a>about</a>
@@ -22,4 +44,7 @@ const Page: NextPage = () => (
   </main>
 );
 
-export default Page;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Page);
