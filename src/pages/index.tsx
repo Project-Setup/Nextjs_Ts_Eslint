1e871a9bc2fb05f40efe44bdf65666acd31c171b
clfhhc <clfhhc@gmail.com>
2020-04-14 00:45:52 -0700

    added link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { NextPage } from 'next';
+import Link from '../features/link/Link';
 
 const IndexPage: NextPage<{ [key in string]?: string }> = ({ ...appProps }) => (
   <div>
     <p>{`Props from _app.tsx: ${JSON.stringify(appProps)}`}</p>
     <p>{`page process env: ${process.env.TEST_PAGE_VAR}`}</p>
+    <br />
+    <Link href="/index/2">
+      <a>index/2</a>
+    </Link>
   </div>
 );
 
