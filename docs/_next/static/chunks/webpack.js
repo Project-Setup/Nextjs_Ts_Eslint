43367b68b73df75ac8ac7bc3e49dc3436a11d18c
clfhhc <clfhhc@gmail.com>
2020-12-28 11:43:38 -0800

    npm upgrade

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bb996de896aed5de8a2e";
+/******/ 	var hotCurrentHash = "87450d10298b3ff83871";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
