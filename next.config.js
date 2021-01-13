15975072ba8e10f46fd8c00b23e0498650f68b78
clfhhc <clfhhc@gmail.com>
2021-01-12 22:54:03 -0800

    add reactStrictMode

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const LINK_PREFIX = process.env.NEXT_PUBLIC_LINK_PREFIX || '';
 
 module.exports = () => ({
   target: 'serverless',
+  reactStrictMode: true,
   poweredByHeader: false,
   basePath: LINK_PREFIX,
 });
